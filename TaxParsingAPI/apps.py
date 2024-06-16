325a93c1dc24c2d646a3f09d8e9e80e5d5b107cf
Achbold Jugdersuren <archiesj@achbolds-mbp.mynetworksettings.com>
2024-06-16 18:38:03 -0400

    initial django project set up

---
+++ TaxParsingAPI/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TaxparsingapiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'TaxParsingAPI'
---
+++ TaxParsingAPI/tests/test_setup.py
@@ -0,0 +1,5 @@
+# test_setup.py
+# This is a basic test to ensure that pytest is correctly set up and working.
+
+def test_basic():
+    assert True
